2140390cb60e837adac6124388c37cf1ea2a2cee
Nelson Chacon <cursodecomputacion.net@gmail.com>
2021-01-25 18:16:26 -0600

    Agregando ejercicio2

---
+++ Ejercicios/Ejercicio2/Mensajes.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+
+int main(int argc, char const *argv[])
+{   string nombre;
+    cout<<"\nCual es su nombre: ";
+    cin>>nombre;
+    cout<<endl;
+    cout<<"Hola mi nombre es: "<<nombre;
+    return 0;
+}
