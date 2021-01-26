fb4b68483ba6bce86c8d07c343e47163d7dd37f3
Nelson Chacon <cursodecomputacion.net@gmail.com>
2021-01-25 19:33:30 -0600

    Variables

---
+++ Ejercicios/Ejercicio3/Variables.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	int NumeroEntero =15;
+	cout<<"\nEl numero entero es: "<<NumeroEntero;
+	NumeroEntero =18;
+	cout<<"\nEl Nuevo numero entero es: "<<NumeroEntero;
+	
+	
+	
+	
+	
+	return 0;
+}
