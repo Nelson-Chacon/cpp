93dceb0b7f899480c370ba04a0eb63ff461a4682
Nelson Chacon <cursodecomputacion.net@gmail.com>
2021-01-28 12:25:49 -0600

    Ejercicio4

---
+++ Ejercicios/Ejercicio4-Todas-Variables/Todas-variables.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    bool ValorBolean =true;
+	int NumeroEntero=15;
+	double ValorDouble =19999.029303;
+	string ValorString = "Hola Mundo";
+	char ValorChar= 'A';
+    /*Mostrar datos*/
+	cout<<ValorBolean<<endl;
+	cout<<NumeroEntero<<endl;
+	cout<<ValorDouble<<endl;
+	cout<<ValorString<<endl;
+	cout<<ValorChar<<endl;
+    return 0;
+}
