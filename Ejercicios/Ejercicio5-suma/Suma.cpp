d5dbd35cea93b06c70643a2370ad96b6d3b74e4f
Nelson Chacon <cursodecomputacion.net@gmail.com>
2021-01-31 13:27:52 -0600

    Suma

---
+++ Ejercicios/Ejercicio5-suma/Suma.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int a=0;
+    int b=0;
+    int resultado;
+    cout<<"\nIngrese el valor de A: ";
+    cin>>a;
+    cout<<"\nIngrese el valor de b: ";
+    cin>>b;
+    resultado=a+b;
+    cout <<"\nLa suma de a + b es: "<<resultado;
+
+
+    return 0;
+}
