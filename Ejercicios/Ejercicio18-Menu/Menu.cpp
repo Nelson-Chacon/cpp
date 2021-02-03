e9a91a10abf30e72d3fc28fc94c89d0973fabba9
Nelson Chacon <cursodecomputacion.net@gmail.com>
2021-02-02 19:33:20 -0600

    Ejercicio 18

---
+++ Ejercicios/Ejercicio18-Menu/Menu.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    int opcion=0;
+    while (true)
+    {
+        cout<<"********"<<endl;
+        cout<<"**MENU**"<<endl;
+        cout<<"********"<<endl;
+
+        cout<<"1- Cafe y granitas"<<endl;
+        cout<<"2- Reposteria"<<endl;
+        cout<<"0- Salir"<<endl;
+        cout<<"Seleccione una opcion: ";
+        cin>>opcion;
+
+        if (opcion==1){
+        system("cls");
+        cout<<"Estas en el menu de cafe y granitas"<<endl;
+        system("pause");
+        }
+        if (opcion==2){
+        system("cls");
+        cout<<"Estas en el menu de reposteria"<<endl;
+        system("pause");
+        }
+        if (opcion==0){
+        break;
+        }   
+    }
+    cout<<"\nSALISTE DEL PROGRAMA";
+
+
+
+    return 0;
+}
+
