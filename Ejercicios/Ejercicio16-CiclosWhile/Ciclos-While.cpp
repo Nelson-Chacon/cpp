57eeda415ccabfae6081e10df90d65196977af2e
Nelson Chacon <cursodecomputacion.net@gmail.com>
2021-02-02 18:15:26 -0600

    Ciclos

---
+++ Ejercicios/Ejercicio16-CiclosWhile/Ciclos-While.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+
+int main(int argc, char const *argv[])
+{
+    int a=5;
+    int b=2;
+
+    while (a>b)
+    {
+        cout<<"\nIngrese un numero: ";
+        cin>>a;
+    }
+    
+    return 0;
+}
