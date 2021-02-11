c8a8065229f4d07fb0aa7e4ffe6323d9e9faff58
Nelson Chacon <cursodecomputacion.net@gmail.com>
2021-02-11 12:15:15 -0600

    Ejercicio39

---
+++ Ejercicios/Ejercicio39-Modulacion/main.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+
+extern int sumar(int a,int b);
+int main(int argc, char const *argv[])
+{
+    system("cls");
+    cout<<sumar(2,3);
+    cout<<endl;
+    return 0;
+}
