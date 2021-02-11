00e3501b67df2e962b400ad5e686e273f8380a39
Nelson Chacon <cursodecomputacion.net@gmail.com>
2021-02-11 12:01:02 -0600

    Ejercicio 36

---
+++ Ejercicios/Ejercicio36/Declaracion.cpp
@@ -0,0 +1,22 @@
+#include <iostream> 
+
+using namespace  std ;
+
+
+int  sumar ( int a, int b);
+
+int  main ( int argc, char  const * argv [])
+{
+    
+    cout << sumar ( 5 , 7 );
+
+   
+
+    return  0 ;
+}
+
+int  sumar ( int a, int b)
+{
+    
+    return a + b;
+}
