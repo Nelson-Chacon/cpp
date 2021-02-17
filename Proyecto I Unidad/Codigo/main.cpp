373d7972a8b9025a154e0db31ce6351ad65a164d
Nelson Chacon <cursodecomputacion.net@gmail.com>
2021-02-16 18:34:14 -0600

    Proyecto unidad1

--- Proyecto I Unidad/Codigo/main.cpp
+++ Proyecto I Unidad/Codigo/main.cpp
@@ -7,9 +7,32 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
     system("cls");
-
-    // starShip();
-    // snake();
-
+    int opcion=0;
+    cout<<"\t******************"<<endl;
+    cout<<"\t**MENU DE JUEGOS**"<<endl;
+    cout<<"\t******************"<<endl;
+    cout<<endl;
+    cout<<"1-> StarShip"<<endl;
+    cout<<"2-> Snake "<<endl;
+    cout<<"Seleccione un juego: ";//pedimos al usuario que seleccione una opcion ingresando un numero entero
+    cin>>opcion;
+    switch (opcion)
+    {
+    case 1:
+    { 
+        system("cls");
+        system ("color a0");
+        starShip();
+        break;
+    }
+    case 2:
+    { 
+        system("cls");
+        system ("color 01");
+        snake();
+        break;
+    }
+    
+    }
     return 0;
 }
